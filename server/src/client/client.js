efc32c7f2c819b330fb57c7e56109e6c730de7a5
shen qin <shenqin@marketnode.com>
2023-08-28 17:37:34 +0800

    feat: 添加监听事件等客户端代码

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,2 +1,6 @@
 // 客户端项目入口
-console.log('hello');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './components/Home';
+
+const root = ReactDOM.hydrateRoot(document.getElementById('root'), <Home />);
--- server/src/index.js
+++ server/src/index.js
@@ -9,11 +9,24 @@ import { renderToString } from 'react-dom/server';
 import Home from './client/components/Home';
 
 const app = express();
+app.use(express.static('public')); // <- 通过这样配置，会自动在bundle.js前自动添加 public/
 
 //这里使用了jsx语法，node环境无法识别，所以需要babel进行编译
 app.get('/', (req, res) => {
   const content = renderToString(<Home />);
-  res.send(content);
+
+  // <- 加载打包后的浏览器端js文件
+  // 添加id为root的div
+  const html = ` 
+  <html>
+    <head></head>
+    <body>
+      <div id="root">${content}</div>
+      <script src="bundle.js"></script>
+    </body>
+  </html>
+  `;
+  res.send(html);
 });
 
 app.listen(3000, () => {
