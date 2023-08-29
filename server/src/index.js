1bdd1e0cf271464d627af2a1a679b855af120156
daodao <valleysq@163.com>
2023-08-29 21:05:38 +0800

    feat: 优化

--- server/src/index.js
+++ server/src/index.js
@@ -3,32 +3,17 @@
 // const React = require('react');
 // const Home = require('./client/components/Home').default;
 
-import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Home from './client/components/Home';
+import express from 'express'
+import renderer from './helpers/renderer'
 
-const app = express();
-app.use(express.static('public')); // <- 通过这样配置，会自动在bundle.js前自动添加 public/
+const app = express()
+app.use(express.static('public')) // <- 通过这样配置，会自动在bundle.js前自动添加 public/
 
 //这里使用了jsx语法，node环境无法识别，所以需要babel进行编译
 app.get('/', (req, res) => {
-  const content = renderToString(<Home />);
-
-  // <- 加载打包后的浏览器端js文件
-  // 添加id为root的div
-  const html = ` 
-  <html>
-    <head></head>
-    <body>
-      <div id="root">${content}</div>
-      <script src="bundle.js"></script>
-    </body>
-  </html>
-  `;
-  res.send(html);
-});
+  res.send(renderer())
+})
 
 app.listen(3000, () => {
-  console.log('listening');
-});
+  console.log('listening')
+})
--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const baseConfig = require('./webpack.base');
+const path = require('path')
+const { merge } = require('webpack-merge')
+const baseConfig = require('./webpack.base')
+const webpackNodeExternals = require('webpack-node-externals')
 
 const config = {
   target: 'node', // 声明是构建nodeJS,如果不声明，默认会认为打包在浏览器环境
@@ -9,8 +10,8 @@ const config = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'), // 打包为/build/bundle,js
   },
-
+  externals: [webpackNodeExternals()],
   mode: 'development',
-};
+}
 
-module.exports = merge(baseConfig, config);
+module.exports = merge(baseConfig, config)
