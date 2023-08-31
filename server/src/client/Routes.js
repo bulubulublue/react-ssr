ac48a61ad9f1dac28586aff549a6bc58c31cd7f9
shen qin <shenqin@marketnode.com>
2023-08-31 15:09:16 +0800

    feat: 添加组件layout

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
 import UsersListPage, { loadData } from './pages/UsersListPage';
+import App from './pages/App';
 
 const createRoutes = store => [
   {
-    path: '/',
-    ...HomePage,
-  },
-  {
-    path: '/users',
-    Component: UsersListPage,
-    loader: () => loadData(store),
+    Component: App,
+    children: [
+      {
+        path: '/',
+        ...HomePage,
+      },
+      {
+        path: '/users',
+        Component: UsersListPage,
+        loader: () => loadData(store),
+      },
+    ],
   },
 ];
 
